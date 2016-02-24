63ffe36098bfc90b6fbfb2cda413d9331f2fb367
akshayaurora <akshayaurora@gmail.com>
2016-02-24 16:29:22 +0530

    use MarkupLabel instead of RstDoc

--- pydelhiapp/main.py
+++ pydelhiapp/main.py
@@ -11,20 +11,7 @@ class PyDelhiApp(App):
 
 	def on_start(self):
 		self.about_text = '''
-**About the conference**\n\n
-PyDelhi Conference is an upcoming conference 
-hosted by PyDelhi Community which focuses on 
-using and developing using the Python programming 
-language. The conference, now in its first year, 
-will be conducted annually by the PyDelhi community. 
-We hope to attract the best Python programmers from 
-across the country and abroad. The main aim of the 
-conference is to bring together startups and established 
-companies, beginners and experts and plethora of global 
-entrepreneurs under one roof. We will be inviting experts 
-from various fields to showcase how Python is used by them 
-and talk about new and upcoming technology.All the participants 
-are expected to follow Code of Conduct'''
+[b]About the conference[/b]\n\nPyDelhi Conference is an upcoming conference hosted by PyDelhi Community which focuses on using and developing using the Python programming language. The conference, now in its first year, will be conducted annually by the PyDelhi community. We hope to attract the best Python programmers from across the country and abroad. The main aim of the conference is to bring together startups and established companies, beginners and experts and plethora of global entrepreneurs under one roof. We will be inviting experts from various fields to showcase how Python is used by them and talk about new and upcoming technology.All the participants are expected to follow Code of Conduct'''
 
 if __name__ == '__main__':
     PyDelhiApp().run()
