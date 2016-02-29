8ef9229ecbeed32f16175beccc492f0f3cc51168
akshayaurora <akshayaurora@gmail.com>
2016-02-29 14:43:13 +0530

    improve interaction and add background_image

--- pydelhiapp/main.py
+++ pydelhiapp/main.py
@@ -20,6 +20,8 @@ from kivy.factory import Factory
 Factory.register('TouchRippleBehavior', module='uix.behaviors')
 Factory.register('TabbedCarousel', module='uix.tabbedcarousel')
 
+#from background_services import update_from_remote_schedule
+
 class PyDelhiApp(App):
     ''' Our main app class
     '''
@@ -35,14 +37,41 @@ class PyDelhiApp(App):
     	return True 
 
     def on_start(self):
-        Clock.schedule_interval(self.calc_time_left, 1)
+    #     Clock.schedule_interval(self.calc_time_left, 1)
+        #self.load_local_schedule()
+        #update_from_remote_schedule(callback=schedule_callback)
+        pass
 
-    def calc_time_left(self, dt):
-        td = datetime(2016, 3, 5, 9) - datetime.now()
-        days = td.days
-        hours, remainder = divmod(td.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        self.time_left = '{}d, {}:{}:{} to go'.format(days, hours, minutes, seconds)
+    def schedule_callback(self, status, data):
+        if status == 'success':
+            # update schedule
+            self.load_data(data)
+
+    def load_local_schedule(self):
+        import json
+        data = json.load(open('data/pydelhi-conf-events.json'))
+        self.load_data(data)
+
+    def load_data(self, data):
+        grid = self.root.ids.grid_audi_3
+        grid.clear_widgets()
+        EventItem = Factory.EventItem
+        gridadd = grid.add_widgets
+        for hall in data.keys():
+            talks = data[hall]
+            for talk in talks:
+                title = talk['schedule-item-date']
+                date = talk['schedule-item-date']
+                text = talk['schedule-item-text']
+                gridadd(EventItem(title=title, time=date, spkrdetail=text))
+
+
+    # def calc_time_left(self, dt):
+    #     td = datetime(2016, 3, 5, 9) - datetime.now()
+    #     days = td.days
+    #     hours, remainder = divmod(td.seconds, 3600)
+    #     minutes, seconds = divmod(remainder, 60)
+    #     self.time_left = '{}d, {}:{}:{} to go'.format(days, hours, minutes, seconds)
 
 if __name__ == '__main__':
     PyDelhiApp().run()
