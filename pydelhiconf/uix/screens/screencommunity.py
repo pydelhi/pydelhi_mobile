1e82c633bbd1c591db9ce15f864712b092a1d974
Shivani Bhardwaj <shivanib134@gmail.com>
2017-03-10 13:10:16 +0530

    Add empty screens

---
+++ pydelhiconf/uix/screens/screencommunity.py
@@ -0,0 +1,8 @@
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+class ScreenCommunity(Screen):
+    Builder.load_string('''
+<ScreenCommunity>
+    name: 'ScreenCommunity'
+        ''')
