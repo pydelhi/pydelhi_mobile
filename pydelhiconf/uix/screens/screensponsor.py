95472394c35d87d9715995bfcea22cc6a1bc39dc
Apoorva <apoorva.srinivasan@lyxellabs.com>
2017-03-12 21:09:56 +0530

    sponsor screen init

--- pydelhiconf/uix/screens/screensponsor.py
+++ pydelhiconf/uix/screens/screensponsor.py
@@ -4,49 +4,85 @@ Display all the logos of the sponsors.
 
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from network import get_data
+from kivy.properties import ObjectProperty
+from kivy.factory import Factory
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Sponsor(BoxLayout):
+     data = ObjectProperty(None)
 
 class ScreenSponsor(Screen):
-
+   
     
     Builder.load_string('''
 <ScreenSponsor>
     name: 'ScreenSponsor'
     BoxLayout
-        padding: dp(12)
         orientation: 'vertical'
         spacing: dp(4)
-        AsyncImage
-            source: 'https://conference.pydelhi.org/img/platinum_sponsor.jpg'
-            allow_stretch: True
-            size_hint: 1, 1
-        BoxLayout
-            orientation: 'vertical'
-            spacing: dp(12)
-            Image
-                allow_stretch: True
-                source: ''
-            BoxLayout
-                orientation: 'vertical'
-                Widget
-                Image
-                    allow_stretch: True
-                    source: ''
-        BoxLayout
-            size_hint_y: None
-            height: dp(50)
-            PyConButton
-                text: 'Sponsor Us'
-                default: True
-                size_hint_y: None
-                height: dp(40)
-                on_release:
-                    import webbrowser
-                    webbrowser.open('https://in.pycon.org/2016/sponsorship-prospectus.pdf')
-            PyConButton
-                text: 'Contact Us'
-                size_hint_y: None
-                height: dp(40)
-                on_release:
-                    import webbrowser
-                    webbrowser.open('mailto:sponsorship@in.pycon.org')
+        id:main
+        
+<Footer@BoxLayout>
+    id:footer
+    size_hint_y: None
+    height: dp(50)
+    PyConButton
+        text: 'Sponsor Us'
+        default: True
+        size_hint_y: None
+        height: dp(40)
+        on_release:
+            import webbrowser
+            webbrowser.open('https://in.pycon.org/2016/sponsorship-prospectus.pdf')
+    PyConButton
+        text: 'Contact Us'
+        size_hint_y: None
+        height: dp(40)
+        on_release:
+            import webbrowser
+            webbrowser.open('mailto:sponsorship@in.pycon.org')
+
+<Sponsor@BoxLayout>
+    orientation: 'vertical'
+    spacing: dp(12)
+    size_hint: 1, 1
+    
+    Label
+        text: self.parent.data['name']
+        
+    AsyncImage
+        size_hint:.8,.8
+        halign: 'center'
+        valign: 'middle'
+        allow_stretch:True
+        source: self.parent.data['logo']
+    
+    Label
+        text: self.parent.data['about']
+        size_hint:1,None
+        padding: dp(5), dp(10)
+        font_size: dp(10)
+        halign:'left'
+        text_size: self.width, None
 ''')
+
+    def on_enter(self, onsuccess=False):
+        '''Series of actions to be performed when Schedule screen is entered
+        '''
+
+        # this should update the file on disk
+        sponsors = get_data('sponsors', onsuccess=onsuccess).get('0.0.1')
+        main_box = self.ids.main;
+
+        for s in sponsors:
+            bl = Factory.Sponsor(size_hint_y=1/len(sponsors), data=s)
+            main_box.add_widget(bl)
+        # footer = self.ids.footer;
+        # main_box.add_widget(footer)
+
+# Label
+#         Text:getattr(self, 'data',{}).get('logo')
+#     Label
+#         Text:getattr(self, 'data',{}).get('logo')
