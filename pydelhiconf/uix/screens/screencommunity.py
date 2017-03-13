711e58fcbfffdf0a6d04c2b9306807745fd10176
Shivani Bhardwaj <shivanib134@gmail.com>
2017-03-13 17:06:16 +0530

    Make community screen

--- pydelhiconf/uix/screens/screencommunity.py
+++ pydelhiconf/uix/screens/screencommunity.py
@@ -1,8 +1,53 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.gridlayout import GridLayout
+from kivy.factory import Factory
 from kivy.lang import Builder
+from functools import partial
+
 
 class ScreenCommunity(Screen):
     Builder.load_string('''
 <ScreenCommunity>
     name: 'ScreenCommunity'
+    ScrollView
+        ScrollGrid
+            id: container
+            AsyncImage
+                source: "atlas://data/default/community"
+                size_hint_y: None
+                allow_stretch: True
+                height: dp(120)
+            BackLabel
+                id: bcklbl
+
+<ImBut@TouchRippleBehavior+ButtonBehavior+Image>
+    text_size: self.size
+    size_hint_y: None
+    mipmap: True
+    height: dp(30)
         ''')
+
+    def on_enter(self, onsuccess=False):
+        from network import get_data
+        community = get_data('community', onsuccess=onsuccess).get('0.0.1')[0]
+
+        if not community:
+            return
+
+        self.ids.bcklbl.text = community['about']
+
+        social_comm = community['social']
+        social_len = len(social_comm)
+
+        gl = GridLayout(cols=social_len,
+                    size_hint_y=None,
+                    padding='2dp',
+                    spacing='2dp')
+        import webbrowser
+        for social_acc, social_link in social_comm.items():
+            imbt = Factory.ImBut()
+            imbt.source = 'atlas://data/default/' + social_acc.lower()
+            imbt.on_released = partial(webbrowser.open, social_link)
+            gl.add_widget(imbt)
+
+        self.ids.container.add_widget(gl)
