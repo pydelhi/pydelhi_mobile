30432055b374059f8270bcd1cd59cd4fcf8f1c9f
akshayaurora <akshayaurora@gmail.com>
2017-08-10 18:53:03 +0530

    setup basic structure of the app.

--- eventsapp/main.py
+++ eventsapp/main.py
@@ -1,4 +1,20 @@
 '''A simple App made for managing and attending events.
+
+Step 1 setting up the basic structure of the app
+
+PyCon-Mobile-App
+|
+|___ LICENSE
+|___ Makefile
+|___ README.md
+|___ eventsapp
+       |___ main.py: "This is the main entry point of our application"
+       |___ uix: "This is where our user interface elements go."
+             |___ 
+|
+|___ tools: "This is where all our tools including assets etc go."
+       |___ images: 'this is where all our image assets go'
+|___ tests: "All our tests go here"
 '''
 
 from kivy.app import App
