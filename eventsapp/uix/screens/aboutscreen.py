14f9215272c93dfeeb98705c316b716e36aae832
akshayaurora <akshayaurora@gmail.com>
2017-09-30 13:41:53 +0530

    include missing files for about screen

---
+++ eventsapp/uix/screens/aboutscreen.py
@@ -0,0 +1,16 @@
+'''
+AboutScreen:
+=============
+Display the logo
+'''
+
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+class AboutScreen(Screen):
+
+   Builder.load_string('''
+<AboutScreen>
+    name: 'AboutScreen'
+    Button
+ ''')
